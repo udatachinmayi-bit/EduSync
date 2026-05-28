bef570964552a4b99f98bb9424818c21b3d97738
Chinmayi <udatachinmayi@gmail.com>
2026-05-28 22:13:33 +0530

    Fixed error localhost

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://edusync-0w0o.onrender.com");
 
 function ChatBox({ roomCode, user }) {
   const [message, setMessage] = useState("");
